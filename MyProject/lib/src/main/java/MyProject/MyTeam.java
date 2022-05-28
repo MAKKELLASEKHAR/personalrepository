96f1e6b21026141ca73a58803131e1e72877b394
Sekhar Makkella <makkella.sekhar@capgemini.com>
2022-05-28 15:26:52 +0530

    Nani-update

---
+++ MyProject/lib/src/main/java/MyProject/MyTeam.java
@@ -0,0 +1,10 @@
+package MyProject;
+
+public class MyTeam {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Shekhar and Yours?");
+
+	}
+
+}
---
+++ practicerepository/lib/src/main/java/practicerepository/OurTeamNames.java
@@ -0,0 +1,10 @@
+package practicerepository;
+
+public class OurTeamNames {
+
+	public static void main(String[] args) {
+		System.out.println("I am Shekhar");
+
+	}
+
+}
